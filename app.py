fd4c5ae4fc3d6cb03bcb8e63c72cbe9d35129e07
hogDuc <nguyengiahongduc@gmail.com>
2025-05-06 16:30:50 +0700

    test commit for dashboard deployment

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# Read data
+revenue_df = pd.read_excel("reti_data.xlsx", sheet_name="revenue", header=None)
+
+st.header("RETI Dashboard")
+st.dataframe(revenue_df)
